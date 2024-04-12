674e9690e114f74ad50f694c992f04e1b4f09aa7
Thijs Janssen van Raaij <thijs@nforza.nl>
2024-04-12 10:42:47 +0200

    logging

--- main.py
+++ main.py
@@ -4,31 +4,33 @@ import json
 import pandas
 import uuid
 
-app = FastAPI()
+# CHANGE THIS TO SOMETHING DIFFERENT
 package = "pandas"
-url = f"https://api.stackexchange.com/2.3/search?order=desc&sort=votes&tagged={package}&site=stackoverflow"
-randomString = str(uuid.uuid4())
+
+app = FastAPI()
+index = 0
+csvList = []
 
 @app.get("/")
 def root():
-  response = httpx.get(url)
+  response = httpx.get(f"https://api.stackexchange.com/2.3/search?order=desc&sort=votes&tagged={package}&site=stackoverflow")
   jsonObject =  json.loads(response.text)
-  csvList = []
   for item in jsonObject["items"]:
-    qurl = f"https://api.stackexchange.com/2.3/questions/{item["question_id"]}/comments?order=desc&sort=creation&site=stackoverflow"
-    qresponse = httpx.get(qurl)
+    qresponse = httpx.get(f"https://api.stackexchange.com/2.3/questions/{item["question_id"]}/comments?order=desc&sort=creation&site=stackoverflow")
     qjsonObject = json.loads(qresponse.text)
+    index += 1
     csvList.append(item["title"])
-    print(f"question: {item["title"]}")
+    print(index)
     for qitem in qjsonObject["items"]:
-      curl = f"https://api.stackexchange.com/2.3/comments/{qitem["comment_id"]}?order=desc&sort=creation&site=stackoverflow&filter=!nNPvSN_ZTx"
-      cresponse = httpx.get(curl)
+      cresponse = httpx.get(f"https://api.stackexchange.com/2.3/comments/{qitem["comment_id"]}?order=desc&sort=creation&site=stackoverflow&filter=!nNPvSN_ZTx")
       cjsonObject = json.loads(cresponse.text)
+      index += 1
       csvList.append(cjsonObject["items"][0]["body"])
-      print(f"comment: {cjsonObject["items"][0]["body"]}")
+      print(index)
     
+  print(f"Added {index} fields")
   dict = {"text": csvList}
   df = pandas.DataFrame(dict)
-  df.to_csv(f'{randomString}.csv', index=False, header=False)
+  df.to_csv(f'{str(uuid.uuid4())}.csv', index=False, header=False)
     
   return csvList
